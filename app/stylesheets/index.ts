8a3951a6ba78ae473f31a96fe560638af647eab2
unknown <eunhoo0903>
2024-07-09 20:42:03 +0900

    공지 페이지 추가하기

---
+++ app/stylesheets/index.ts
@@ -0,0 +1,129 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+    width: 100%;
+    max-width: 1500px;
+    margin: 180px auto 50px auto;
+    display: flex;
+    justify-content: center;
+`;
+
+export const NoticeListBackground = styled.div`
+    width: 300px;
+    height: 650px;
+    border: 1px solid #999;
+    background-color: #FFFFFF;
+    border-radius: 7px;
+    margin: 10px;
+    padding: 10px;
+`;
+
+export const NoticeHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const NoticeListDetail = styled.div`
+    width: 1200px;
+    height: 650px;
+    border: 1px solid #999;
+    background-color: #FFFFFF;
+    border-radius: 7px;
+    margin: 10px;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const NoticeListText = styled.strong`
+    font-size: 20px;
+`;
+
+export const NoticeListDetailContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+`;
+
+export const NoticeListDetailImg = styled.img`
+    max-width: 100%;
+    height: auto;
+`;
+
+export const NoticeListDetailP = styled.p`
+    font-size: 20px;
+    font-weight: 700;
+    margin-top: 10px;
+`;
+
+export const NoticeListA = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+    border: 1.5px solid #000;
+    border-radius: 4px;
+`;
+
+export const PlusButton = styled.span`
+    font-size: 15px;
+`
+
+export const NoticeForm = styled.form`
+    width: 100%;
+    height: 550px;
+    border: 1px solid #999;
+    background-color: #F7F7F7;
+    border-radius: 7px;
+    margin: 12px;
+    margin-top: 70px;
+`;
+
+export const NoticeTitleInput = styled.input`
+    width: 100%;
+    max-width: 900px;
+    height: 50px;
+    margin-top: 10px;
+    font-size: 25px;
+`
+
+export const NoticeBodyInput = styled.textarea`
+    font-size: 17px;
+    width: 100%;
+    max-width: 900px;
+    height: 400px;
+    margin-top: 10px;
+    resize: none;
+    font-weight: bold;
+`
+
+export const NoticeFormWrite = styled.button`
+    margin-top: 20px;
+    background: #6AD37B;
+    border-radius: 3px;
+    color: #FFFFFF;
+    width: 70px;
+`
+
+export const NoticeBody = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const NoticeList = styled.div`
+    width: 100%;
+    max-width: 250px;
+    height: 100px;
+    border: 1px solid #999;
+    background-color: #F7F7F7;
+    border-radius: 7px;
+    margin: 10px;
+    padding: 10px;
+`
